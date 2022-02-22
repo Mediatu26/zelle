767cd0724117eee784930a38c6b6060253c06b7e
Mediatu Kamara <mediatukamara@gmail.com>
2022-02-22 02:32:41 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+ echo "Mediaty Kamara"
+Mediaty Kamara
+[ec2-user@ip-172-31-7-95 ~]$ echo passwd
+passwd
+[ec2-user@ip-172-31-7-95 ~]$ echo "welcome to zelle application"
+welcome to zelle application
+
